0a9e190539eff8333c2d2c596ea126cc129dafe5
Arian <arianamani.sec@gmail.com>
2021-03-27 21:46:05 +0430

    Init

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from PIL import Image, ImageFilter
+
+
+def JPG_to_PNG(directory1, directory2):
+    """
+    Give two directories as entries and it will convert all pictures in the first directory from JPG to PNG and save them in the second directory
+    """
+
+    # source_path = os.
+
+def main():
+    JPG_to_PNG(sys.argv[1], sys.argv[2])
+
+
+if __name__ == '__main__':
+    main()
