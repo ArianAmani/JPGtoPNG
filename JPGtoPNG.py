3d157dc46d46f137eeb7e858f472811151b9a999
ArianAmani <arianamani.sec@gmail.com>
2021-03-27 22:35:09 +0430

    added README.md

--- JPGtoPNG.py
+++ JPGtoPNG.py
@@ -1,11 +1,12 @@
 import sys
 import os
-from PIL import Image, ImageFilter
+from PIL import Image
 
 
 def JPG_to_PNG(source_path, target_path):
     """
-    Give two directories as entries and it will convert all pictures in the first directory from JPG to PNG and save them in the second directory
+    Give two directories as entries and it will convert all pictures in the first directory from JPG to PNG and save
+    them in the second directory
     """
     count = 0
     if not source_path.endswith('/'):
@@ -15,7 +16,7 @@ def JPG_to_PNG(source_path, target_path):
         target_path = target_path + '/'
 
     for file_name in os.listdir(source_path):
-        if file_name.endswith(".jpg"):
+        if file_name.lower().endswith(".jpg"):
             img_path = source_path + file_name
             img = Image.open(img_path)
             if not os.path.exists(target_path):
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JPGtoPNG
+
+Script will loop through files in source directory and converts .jpg files to .png files and saves them in target directory.
+> python3 JPGtoPNG.py source_directory target_directory
